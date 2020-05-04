a3aa1503583c07d03bca2c2d13c4a367ea9fff17
yuorme <yuorme@gmail.com>
2020-05-03 22:18:02 -0400

    added plotly_config

--- config.py
+++ config.py
@@ -1,3 +1,26 @@
 app_config = {
     'debug' : False, #Set flask debug mode: True for development, False for production
 }
+
+plotly_config = dict(
+    scrollZoom = True,
+    displaylogo= False,
+    showLink = False,
+    toImageButtonOptions={
+        'format':'png',
+        'filename':'covid-projections',
+        #twitter optimized 16:9 size
+        'width':1200,
+        'height':675
+    },
+    modeBarButtonsToRemove = [
+        'sendDataToCloud',
+        'zoomIn2d',
+        'zoomOut2d',
+        'hoverClosestCartesian',
+        'hoverCompareCartesian',
+        'hoverClosest3d',
+        'hoverClosestGeo',
+        'resetScale2d'
+    ],
+)
