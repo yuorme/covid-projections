08450249e4d0dd485aad39e4a4c5f1af9cf77d91
Patrick Vossler <patrick.vossler18@gmail.com>
2020-05-30 13:38:17 -0700

    Revert "add flask cache to requirements.txt"
    This reverts commit 65972203

--- config.py
+++ config.py
@@ -1,9 +1,7 @@
 import os
-from dotenv import load_dotenv
-load_dotenv()
 
 app_config = {
-    'debug' : True, #Set flask debug mode: True for development, False for production
+    'debug' : False, #Set flask debug mode: True for development, False for production
     'sqlalchemy_database_uri' : f'postgresql+psycopg2://postgres:{os.environ.get("COVID_PRED_POSTGRES_PASS")}@'\
                                     f'{os.environ.get("COVID_PRED_RDS_URL")}/covid_projections?sslmode=verify-ca'\
                                 f'&sslrootcert=rds-ca-2019-root.pem',
