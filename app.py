012787f7fc17a54f5dba38af63bfc8798b874686
Patrick Vossler <patrick.vossler18@gmail.com>
2020-06-03 13:24:28 -0700

    Remove cache import

--- app.py
+++ app.py
@@ -13,7 +13,6 @@ import dash_core_components as dcc
 import dash_bootstrap_components as dbc
 import dash_html_components as html
 from dash.dependencies import Input, Output, State
-from flask_caching import Cache
 from flask_talisman import Talisman
 
 import plotly.graph_objects as go
