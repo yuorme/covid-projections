b8bc333fbae12ac72678d59e2e232146fe0f9fb8
D. David Yu <yuorme@gmail.com>
2020-09-25 08:56:10 -0400

    Update app.py
    Changed default min_date to 60 days from 30

--- app.py
+++ app.py
@@ -314,7 +314,7 @@ controls = dbc.Card(
                     id='model-date-picker',
                     min_date_allowed=min_date,
                     max_date_allowed=datetime.today(),
-                    start_date=datetime.today() - timedelta(days=30), #HACK: Temporarily fixes the colorscale issue for >12 models
+                    start_date=datetime.today() - timedelta(days=60), #HACK: Temporarily fixes the colorscale issue for >12 models
                     end_date=datetime.today(),
                     initial_visible_month=datetime.today(),
                 ),
